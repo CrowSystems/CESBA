fc586da3e742cc0044dd63daa58fa8bd203a2727
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-11-16 13:35:42 -0600

    Programa de Piedra Papel o Tijera

--- Compiladores/20241109-Clase Python/04PiedraPapelTijera.py
+++ Compiladores/20241109-Clase Python/04PiedraPapelTijera.py
@@ -1,8 +1,30 @@
 import random
 
 def juego_piedra_papel_tijera():
-    opciones = ["piedra", "piedra", "tijera"]
-    puntuacion_jugador = 0
-    puntuacion_computadora = 0
+opciones = ["piedra", "piedra", "tijera"]
+puntuacion_jugador = 0
+puntuacion_computadora = 0
 
-    print("Bienvenido al juego de piedra, Papel o Tijera!")
+print("Bienvenido al juego de piedra, Papel o Tijera!")
+print("Juega ´piedra', 'papel' o 'tijera'. Escribe 'salir' para terminar el juego.")
+
+while True:
+    jugador = input("Elige tu opción: ").lower()
+    if jugador == "salir":
+        print("Gracias por jugar. ¡Hasta luego!")
+        break
+    if jugador not in opciones:
+        print("Opción no valida. Por Favor, elige 'piedra', 'papel' o 'tijera'.")
+        continue
+
+    computadora = random.choice(opciones)
+    print(f"La computadora eligio: {computadora}")
+
+    if jugador == computadora:
+        print("¡Es un empate!")
+    elif(jugador == "piedra" and computadora == "tijera") or \
+        (jugador == "papel" and computadora == "piedra") or \
+        (jugador == "tijera" and computadora == "papel"):
+
+        print("Ganaste esta ronda!")
+        puntuacio
